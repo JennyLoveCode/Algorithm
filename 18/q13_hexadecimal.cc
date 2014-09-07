6e5a4f2778b2b61f4dc6189acec701e41e681e3e
JennyLoveCode <zhangjianisapplying@gmail>
2014-09-07 14:19:59 -0500

    强化练习1

--- 18/q13_hexadecimal.cc
+++
@@ -1,9 +0,0 @@
-/*
-Q13 (binary) Given a number x, how to get the hexadecimal representation 
-of the number in
-string type? E.g 29 ⇒ “0x1D
-10 进制 ＝>0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-16 进制 => 0 1 2 3 4 5 6 7 8 9 A B C D E
-*/
-#include <iostream>
-#include <string>
